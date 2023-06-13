7bd8257a96228293b1e2922c3e5ee1bb59ea8aff
sooominnn <lsm980830@gmail.com>
2023-06-13 20:19:24 +0900

    스프링 부트 게시판 프로젝트 시작

---
+++ src/main/java/com/example/soominprojectboard/SoominProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.soominprojectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoominProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoominProjectBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/soominprojectboard/SoominProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soominprojectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoominProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
